fa53605d032362687ae5d63516e70b045e6f3700
school01321 <80627249+school01321@users.noreply.github.com>
2021-04-06 11:48:03 +0900

    0406

---
+++ Assets/Scripts/ScollingObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScollingObject : MonoBehaviour
+{
+    public float speed = 10f;
+
+    void Update()
+    {
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+    }
+}
